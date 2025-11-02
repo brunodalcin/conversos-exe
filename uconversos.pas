ea249feda6fcdb7677147d67bec3c3cfce45b8d0
Bruno Dalcin <brunodalcin5@gmail.com>
2025-11-02 13:48:20 -0300

    design and combobox adjusts

--- uconversos.pas
+++ uconversos.pas
@@ -6,19 +6,25 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, FileCtrl, StdCtrls,
-  ExtCtrls, ComCtrls, Buttons, ATPanelColor, ATPanelSimple, ATListbox,
-  RTTICtrls;
+  ExtCtrls, ComCtrls, Buttons, Arrow, ATPanelColor, ATPanelSimple, ATListbox,
+  RTTICtrls,
+  //converters
+  FPImage,
+  FPReadBMP, FPWriteBMP,
+  FPReadJPEG, FPWriteJPEG,
+  FPReadPNG, FPWritePNG;
 
 type
 
   { TFrmConverterCompressor }
 
   TFrmConverterCompressor = class(TForm)
-    btnConverter1: TBitBtn;
+    arrow: TArrow;
+    btnCompress: TPanel;
     cbFrom: TComboBox;
     cbTo: TComboBox;
     lbClose: TLabel;
-    lbDropImages: TLabel;
+    lbDrop: TLabel;
     lbFrom: TLabel;
     lbTo: TLabel;
     listbImages: TListBox;
@@ -29,14 +35,16 @@ type
     pgControl: TPageControl;
     tsCompressor: TTabSheet;
     tsConverter: TTabSheet;
+    procedure arrowClick(Sender: TObject);
+    procedure btnConvertClick(Sender: TObject);
     procedure btnConvertEnter(Sender: TObject);
     procedure btnConvertMouseEnter(Sender: TObject);
     procedure btnConvertMouseLeave(Sender: TObject);
+    procedure cbFromChange(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure FormDropFiles(Sender: TObject; const FileNames: array of string);
     procedure FormKeyPress(Sender: TObject; var Key: char);
-
-
+    procedure pgControlChange(Sender: TObject);
 
   private
     const ESC = #27;
@@ -46,6 +54,8 @@ type
     const border = '#4A453E';
     const highlight = '#EAC65A';
     function HexToColor(Hex: string): TColor;
+    procedure AttToCombobox;
+    //procedure Convert(const InputFile, OutputFile : string);
   public
 
   end;
@@ -65,7 +75,7 @@ begin
   panel.Color:= HexToColor(panelBg);
   listbImages.Color := HexToColor(dropBg);
   listbImages.Font.Color := HexToColor(txt);
-  lbDropImages.Font.Color := HexToColor(txt);
+  lbDrop.Font.Color := HexToColor(txt);
   lbClose.Font.Color := HexToColor(txt);
   panelConverter.Color := HexToColor(dropBg);
   panelCompressor.Color := HexToColor(dropBg);
@@ -77,10 +87,29 @@ begin
   lbTo.Font.Color := HexToColor(txt);
   btnConvert.Font.Color := HexToColor(txt);
   btnConvert.Color := HexToColor(dropBg);
+  btnCompress.Font.Color := HexToColor(txt);
+  btnCompress.Color := HexToColor(dropBg);
+  arrow.ArrowColor := HexToColor(txt);
+  arrow.Color := HexToColor(PanelBg);
 
+  cbFrom.Items.AddStrings(['PNG', 'JPEG', 'BMP']);
+  cbFrom.ItemIndex := 0;
+  AttToCombobox;
 end;
 
 procedure TFrmConverterCompressor.btnConvertEnter(Sender: TObject);
+begin
+
+end;
+
+procedure TFrmConverterCompressor.btnConvertClick(Sender: TObject);
+var
+  file_From, file_To: TFPCustomImage;
+begin
+  //Convert()
+end;
+
+procedure TFrmConverterCompressor.arrowClick(Sender: TObject);
 begin
 
 end;
@@ -93,6 +122,12 @@ end;
 procedure TFrmConverterCompressor.btnConvertMouseLeave(Sender: TObject);
 begin
   (Sender as TPanel).Color := HexToColor(dropBg);
+end;
+
+procedure TFrmConverterCompressor.cbFromChange(Sender: TObject);
+begin
+  //when changing FROM combobox, the TO cannot have the same value ex FROM 'jpg' TO 'jpg'
+  AttToCombobox;
 end;
 
 procedure TFrmConverterCompressor.FormDropFiles(Sender: TObject;
@@ -116,6 +151,14 @@ begin
     Close;
 end;
 
+procedure TFrmConverterCompressor.pgControlChange(Sender: TObject);
+begin
+  if pgControl.ActivePageIndex = 0 then
+    lbDrop.Caption := 'Drop Images'
+  else
+    lbDrop.Caption := 'Drop Files';
+end;
+
 
 function TFrmConverterCompressor.HexToColor(Hex: string): TColor;
 var
@@ -131,6 +174,36 @@ begin
   Result := RGBToColor(R, G, B);
 end;
 
+procedure TFrmConverterCompressor.AttToCombobox;
+var
+  i: Integer;
+begin
+  cbTo.Items.BeginUpdate;
+  try
+    // always clear to update
+    cbTo.Items.Clear;
+    for i := 0 to cbFrom.Items.Count - 1 do
+    begin
+      // do not add same IMAGETYPE as selected EX: FROM png TO png
+      if cbFrom.Items[i] <> cbFrom.Text then
+        cbTo.Items.Add(cbFrom.Items[i]);
+    end;
+    // select first index
+    if cbTo.Items.Count > 0 then
+      cbTo.ItemIndex := 0;
+  finally
+    cbTo.Items.EndUpdate;
+  end;
+end;
+
+{procedure TFrmConverterCompressor.Convert(const InputFile, OutputFile : string);
+var
+  Img: TFPMemoryImage;
+  Reader: TFPCustomImageReader;
+  Writer: TFPCustomImageWriter;
+begin
+
+end;}
 
 end.
 
